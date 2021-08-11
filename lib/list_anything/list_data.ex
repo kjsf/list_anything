de5523218c0ceed64605aa2e8db98e4e556de1b7
kjsf <kjsfajutagana@gmail.com>
2021-08-11 17:12:02 +0800

    Add operations to the server
    Added create, add and delete functionalities to the list server.
    Created lists struct to provide structure, should add more info.
    Learned how to traverse nested data structures with Access and
    some Kernel modules like update_in and put_in.

---
+++ lib/list_anything/list_data.ex
@@ -0,0 +1,5 @@
+defmodule ListAnything.ListData do
+  defstruct entries: []
+end
+
+# TODO: Add more list info.
--- lib/list_anything/list_server.ex
+++ lib/list_anything/list_server.ex
@@ -1,6 +1,7 @@
 defmodule ListAnything.ListServer do
   @name :list_server
   use GenServer
+  alias ListAnything.ListData
 
   def start_link() do
     GenServer.start(__MODULE__, :ok, name: @name)
@@ -8,28 +9,54 @@ defmodule ListAnything.ListServer do
 
   @impl true
   def init(:ok) do
-    {:ok, []}
+    {:ok, %{}}
   end
 
-  def get_list() do
-    GenServer.call(@name, :get_list)
+  def create_list(title) do
+    GenServer.call(@name, {:create_list, title})
+  end
+
+  def get_list_all() do
+    GenServer.call(@name, :get_list_all)
   end
 
   # Client Side
-  def add_entry(entry) do
-    GenServer.call(@name, {:add_entry, entry})
+  def add_entry(list_title, entry \\ []) do
+    GenServer.call(@name, {:add_entry, list_title, entry})
+  end
+
+  def remove_entry(list_title, entry \\ []) do
+    GenServer.call(@name, {:remove_entry, list_title, entry})
   end
 
   # Server Side
 
   @impl true
-  def handle_call(:get_list, _from, state) do
+  def handle_call(:get_list_all, _from, state) do
     {:reply, state, state}
   end
 
-  def handle_call({:add_entry, entry}, _from, state) do
-    new_state = [entry | state]
+  def handle_call({:create_list, title}, _from, state) do
+    new_state = Map.put_new(state, title, %ListData{})
 
-    {:reply, "#{entry} added to list", new_state}
+    {:reply, "#{title} list created", new_state}
+  end
+
+  def handle_call({:add_entry, list_title, entry}, _from, state) do
+    new_state =
+      update_in(state, [list_title, Access.key(:entries)], fn entries ->
+        [entry | entries]
+      end)
+
+    {:reply, "#{entry} added to list #{list_title}", new_state}
+  end
+
+  def handle_call({:remove_entry, list_title, entry}, _from, state) do
+    new_state =
+      update_in(state, [list_title, Access.key(:entries)], fn entries ->
+        List.delete(entries, entry)
+      end)
+
+    {:reply, "#{entry} added to list #{list_title}", new_state}
   end
 end
