2b091fb3e55a76bdda0cff7b39c2fab69ff2591c
kjsf <kjsfajutagana@gmail.com>
2021-08-12 18:59:47 +0800

    Separate client and server side code
    Cleaned up function names and parameters for better readability.

--- lib/list_anything/list_server.ex
+++ lib/list_anything/list_server.ex
@@ -1,62 +1,81 @@
 defmodule ListAnything.ListServer do
   @name :list_server
+
   use GenServer
   alias ListAnything.ListData
 
-  def start_link() do
+  # Client Side
+
+  def start_link(_args) do
     GenServer.start(__MODULE__, :ok, name: @name)
   end
+
+  def create(title) do
+    GenServer.call(@name, {:create, title})
+  end
+
+  def get(title) do
+    GenServer.call(@name, {:get, title})
+  end
+
+  def get_all() do
+    GenServer.call(@name, :get_all)
+  end
+
+  def add_entry(title, entry) do
+    GenServer.call(@name, {:add_entry, title, entry})
+  end
+
+  def remove_entry(title, entry) do
+    GenServer.call(@name, {:remove_entry, title, entry})
+  end
+
+  # Server Side
 
   @impl true
   def init(:ok) do
     {:ok, %{}}
   end
 
-  def create_list(title) do
-    GenServer.call(@name, {:create_list, title})
-  end
-
-  def get_list_all() do
-    GenServer.call(@name, :get_list_all)
-  end
-
-  # Client Side
-  def add_entry(list_title, entry \\ []) do
-    GenServer.call(@name, {:add_entry, list_title, entry})
-  end
-
-  def remove_entry(list_title, entry \\ []) do
-    GenServer.call(@name, {:remove_entry, list_title, entry})
-  end
-
-  # Server Side
-
   @impl true
-  def handle_call(:get_list_all, _from, state) do
+  def handle_call(:get_all, _from, state) do
     {:reply, state, state}
   end
 
-  def handle_call({:create_list, title}, _from, state) do
+  def handle_call({:get, title}, _from, state) do
+    found_list = get_in(state, [title, Access.key(:entries), Access.all()])
+    {:reply, found_list, state}
+  end
+
+  def handle_call({:create, title}, _from, state) do
     new_state = Map.put_new(state, title, %ListData{})
 
     {:reply, "#{title} list created", new_state}
   end
 
-  def handle_call({:add_entry, list_title, entry}, _from, state) do
+  def handle_call({:add_entry, title, entry}, _from, state) do
     new_state =
-      update_in(state, [list_title, Access.key(:entries)], fn entries ->
+      update_in(state, [title, Access.key(:entries)], fn entries ->
         [entry | entries]
       end)
 
-    {:reply, "#{entry} added to list #{list_title}", new_state}
+    {:reply, "#{entry} added to the list #{title}", new_state}
   end
 
-  def handle_call({:remove_entry, list_title, entry}, _from, state) do
+  def handle_call({:remove_entry, title, entry}, _from, state) do
     new_state =
-      update_in(state, [list_title, Access.key(:entries)], fn entries ->
+      update_in(state, [title, Access.key(:entries)], fn entries ->
         List.delete(entries, entry)
       end)
 
-    {:reply, "#{entry} added to list #{list_title}", new_state}
+    {:reply, "#{entry} removed from the list #{title}", new_state}
   end
 end
+
+# alias ListAnything.ListServer
+
+# ListServer.start_link([])
+
+# ListServer.create("hello") |> IO.puts()
+# ListServer.add_entry("hello", ["world", "kez"]) |> IO.inspect()
+# ListServer.get("hello") |> IO.inspect()
