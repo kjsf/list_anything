b804ca9eca251e79703d6b258012832a2871eee9
kjsf <kjsfajutagana@gmail.com>
2021-08-13 21:27:10 +0800

    Add docs to the server module and client functions

--- lib/list_anything/list_server.ex
+++ lib/list_anything/list_server.ex
@@ -1,4 +1,7 @@
 defmodule ListAnything.ListServer do
+  @moduledoc """
+    A GenServer that handles operations and keeps the list data for the application.
+  """
   @name :list_server
 
   use GenServer
@@ -6,26 +9,42 @@ defmodule ListAnything.ListServer do
 
   # Client Side
 
+  @doc false
   def start_link(_args) do
     GenServer.start(__MODULE__, :ok, name: @name)
   end
 
+  @doc """
+    Creates a list with given `title`.
+  """
   def create(title) do
     GenServer.call(@name, {:create, title})
   end
 
+  @doc """
+    Returns the contents of the list with the given `title`.
+  """
   def get(title) do
     GenServer.call(@name, {:get, title})
   end
 
+  @doc """
+    Returns all available lists and their contents.
+  """
   def get_all() do
     GenServer.call(@name, :get_all)
   end
 
+  @doc """
+    Creates `entry` to the given list `title`.
+  """
   def add_entry(title, entry) do
     GenServer.call(@name, {:add_entry, title, entry})
   end
 
+  @doc """
+    Deletes `entry` from the given list `title`.
+  """
   def remove_entry(title, entry) do
     GenServer.call(@name, {:remove_entry, title, entry})
   end
@@ -43,6 +62,7 @@ defmodule ListAnything.ListServer do
   end
 
   def handle_call({:get, title}, _from, state) do
+    # May be useful references for someone in the future.
     # entries = get_in(state, [title, Access.key(:entries), Access.all()])
 
     %{^title => %{entries: entries}} = state
